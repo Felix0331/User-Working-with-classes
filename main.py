1fba0fb6ad215783d5abf250d1135f2172d35426
Felix Almejo <fd.almejo@gmail.com>
2021-08-02 17:40:44 -0700

    chained the class methods

--- main.py
+++ main.py
@@ -4,38 +4,31 @@ class User:
 
     def make_deposit(self, amount):
         self.balance += amount
+        return self
     
     def make_withdrawal(self,amount):
         self.balance -= amount
+        return self
 
     def display_user_balance(self):
         print(self.balance)
-    
+        return self
+        
     def transfer_money(self, amount, user):
         self.balance -= amount
         user.balance += amount
+        return self
+
 
 felix = User()
 jess = User()
 elijah = User()
 
-felix.make_deposit(100)
-felix.make_deposit(100)
-felix.make_deposit(100)
-felix.make_withdrawal(50)
-felix.display_user_balance()
+felix.make_deposit(100).make_deposit(100).make_deposit(100).make_withdrawal(50).display_user_balance()
 
-jess.make_deposit(300)
-jess.make_deposit(100)
-jess.make_withdrawal(50)
-jess.make_withdrawal(50)
-jess.display_user_balance()
+jess.make_deposit(300).make_deposit(100).make_withdrawal(50).make_withdrawal(50).display_user_balance()
 
-elijah.make_deposit(200)
-elijah.make_withdrawal(50)
-elijah.make_withdrawal(50)
-elijah.make_withdrawal(50)
-elijah.display_user_balance()
+elijah.make_deposit(200).make_withdrawal(50).make_withdrawal(50).make_withdrawal(50).display_user_balance()
 
 felix.transfer_money(10000,elijah)
 elijah.display_user_balance()
